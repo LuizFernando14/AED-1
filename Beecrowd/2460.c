4e38411af98c632c61791914c148173f41ccd375
LuizFernando14 <157256831+LuizFernando14@users.noreply.github.com>
2024-11-12 20:40:38 -0300

    Create 2460.c

---
+++ Beecrowd/2460.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct cel {
+    int conteudo[1];
+    struct cel *seg;
+};
+
+typedef struct cel celula;
+
+void insert(*fila_inicial, *fila_final){
+    celula *oof;
+    oof->fila_inicial;
+    while(oof != NULL){
+        oof->*seg;
+    }
+}
+ 
+int main() {
+    int 
+    celula *fila_inicial, *fila_final;
+    
+    fila_inicial = malloc(sizeof(celula));
+    fila_inicial->seg = NULL;
+    
+    fila_final = malloc(sizeof(celula));
+    fila_final->seg = NULL;
+    
+    
+ 
+    return 0;
+} //Incompleto
