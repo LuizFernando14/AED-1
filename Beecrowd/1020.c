b678001ea91034e0a0ca7495bd4a96e5249124b0
LuizFernando14 <157256831+LuizFernando14@users.noreply.github.com>
2024-11-12 19:17:16 -0300

    Create 1020.c

---
+++ Beecrowd/1020.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    int age, y, m, d;
+    scanf("%d", &age);
+    y=age/365;
+    m=(age-y*365)/30;
+    d=age-(y*365+m*30);
+    printf("%d ano(s)\n", y);
+    printf("%d mes(es)\n", m);
+    printf("%d dia(s)\n", d);
+    return 0;
+}
