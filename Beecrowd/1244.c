f9f1359cc4570654f17dfff4062488c7524e73a6
LuizFernando14 <157256831+LuizFernando14@users.noreply.github.com>
2024-12-07 21:13:02 -0300

    Update 1244.c

--- Beecrowd/1244.c
+++ Beecrowd/1244.c
@@ -10,60 +10,79 @@ struct cel{
 
 typedef struct cel arvore;
 
-void BinaryTree(struct cel *arvore, char substr[]){
-    struct cel *nova, *f, *p;
+void BinaryTree(arvore **root, char str[]){
+    arvore *nova;
     nova = malloc(sizeof(arvore));
-    nova->chave = strlen(substr);
+    nova->chave = strlen(str);
     nova->esq = NULL;
     nova->dir = NULL;
-    
-    f = &arvore;
-    while (f != NULL) {
-        p = f;
-        if (f->chave > nova->chave)
-            f = f->esq;
+    strcpy(nova->substr, str);
+    arvore *f = *root, *p;
+    if(*root == NULL)
+        *root = nova;
+    else{
+        while (f != NULL) {
+            p = f;
+            if (f->chave >= nova->chave)
+                f = f->esq;
+            else{
+                f = f->dir;
+            }
+        }
+        if (p->chave >= nova->chave)
+            p->esq = nova;
         else{
-            f = f->dir;
+            p->dir = nova;
         }
     }
-    if (p->chave > nova->chave)
-        p->esq = nova;
-    else{
-        p->dir = nova;
+}
+
+void freeTree(arvore *root){
+    if(root != NULL){
+        freeTree(root->esq);
+        freeTree(root->dir);
+        free(root);
     }
 }
 
 void Erd (struct cel *arvore) {
     if (arvore != NULL) {
         Erd (arvore->dir);
-        printf ("%s", arvore->substr);
+        printf ("%s ", arvore->substr);
         Erd (arvore->esq);
     }
 }
 
 int main(){
-    struct cel arvore;
+    arvore *root= NULL;
     char V1[50], V2[50];
-    int n, i=0, m, j=0;
+    int n, i=0, m, j, k=0;
     scanf("%d", &n);
     getchar();
     while(i < n){
         fgets(V1, 50, stdin);
         m = strlen(V1);
+        j = 0;
+        k = 0;
         while(j <= m){
-            if(V1[j] == ' '){
-                V2[j] = '\0';
-                BinaryTree(&arvore, V2);
+            if(V1[j] == ' ' || V1[j] == '\0' || V1[j] == '\n'){
+                V2[k] = '\0';
+                BinaryTree(&root, V2);
                 j++;
+                k=0;
             }
             else{
-                V2[j] = V1[j];
+                V2[k] = V1[j];
                 j++;
+                k++;
             }
         }
-        Erd(arvore);
+        Erd(root);
+        freeTree(root);
+        root = NULL;
+        printf("\n");
+        i++;
     }
 
     return 0;
 }
-//Incompleto
