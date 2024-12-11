e9e547cbf3b142756d99d556ad20f092f4e9ba6e
LuizFernando14 <157256831+LuizFernando14@users.noreply.github.com>
2024-12-11 11:22:35 -0300

    Update 1110.c

--- Beecrowd/1110.c
+++ Beecrowd/1110.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct cel{
     int card;
@@ -8,27 +9,56 @@ struct cel{
 
 typedef struct cel deck;
 
-void Stack(int y, celula *p){
-    celula *nova;
-    nova = malloc(sizeof(celula));
+void Stack(int y, deck *p){
+    deck *nova;
+    nova = malloc(sizeof(deck));
     nova->card = y;
     nova->seg = p->seg;
     p->seg = nova;
 }
 
-void Cicle(celula *p){
-    celula *aux;
-    while(aux->seg != NULL){
-        aux->seg;
+int CardRemaining(deck *r){
+    if(r->seg == NULL) return -1;
+    while(r->seg->seg != NULL){
+        deck* lixo = r->seg;
+        r->seg = lixo->seg;
+        if(lixo->card == 1){
+            printf(" %d", lixo->card);
+        }
+        else{
+            printf(", %d", lixo->card);
+        }
+        free(lixo);
+        deck* atual = r->seg;
+        r->seg = atual->seg;
+        atual->seg = NULL;
+        deck* aux = r;
+        while(aux->seg != NULL){
+            aux = aux->seg;
+        }
+        aux->seg = atual;
     }
-    aux->seg = p->seg;
-}
-
-int CardRemaining(celula *p){
-    
+    return r->seg->card;
 }
 
 int main() {
+    int n, i;
+    deck *p;
+    while(1){
+        p = malloc(sizeof(deck));
+        p->seg = NULL;
+        scanf("%d", &n);
+        if(n == 0)
+            break;
+        for(i = n; i >= 1; i--){
+            Stack(i, p);
+        }
+        printf("Discarded cards:");
+        int j = CardRemaining(p);
+        getchar();
+        printf("\nRemaining card: %d\n", j);
+        free(p);
+    }
 
     return 0;
-} //Incompleto
+}
