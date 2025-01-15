654624b009d4b1bba62fc68bbb7d15eadf3c84b8
LuizFernando14 <157256831+LuizFernando14@users.noreply.github.com>
2025-01-15 11:07:24 -0300

    Update 1855.c

--- Beecrowd/1855.c
+++ Beecrowd/1855.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
  
 int main() {
-    int w, h;
+    int w, h, i, j;
     scanf("%d", &w);
     scanf("%d", &h);
     if(w < 100 && h < 100){
         int MAP1[h][w], m = 0;
         char MAP2[h][w];
-        for(int i = 0; i < h; i++){
-            for(int j = 0; j < w; j++){
+        for(i = 0; i < h; i++){
+            for(j = 0; j < w; j++){
                 scanf("%c", &MAP2[i][j]);
                 MAP1[i][j] = 0;
                 if(MAP2[i][j] == '*')
@@ -17,42 +17,56 @@ int main() {
         }
         if(m != 1){
             printf("!\n");
-            break;
         }
-        j = 0;
-        i = 0;
-        while(i >= 0 && i < h && j >= 0 && j < w){
-            int n;
-            switch(MAP2[i][j]){
-                case '*':
-                    printf("*\n");
+        else{
+            j = 0;
+            i = 0;
+            while(i >= 0 && i < h && j >= 0 && j < w){
+                int n = 0;
+                if(MAP1[i][j] != 0){
+                    printf("!\n");
                     break;
-                case '>':
-                    j++;
-                    n = 1;
-                case '<':
-                    j--;
-                    n = 2;
-                case '^':
-                    i--;
-                    n = 3;
-                case 'v':
-                    i++;
-                    n = 4;
-                case '.':
-                    if(n == 1)
+                }
+                MAP1[i][j] += 1;
+                switch(MAP2[i][j]){
+                    case '*':
+                        printf("*\n");
+                        break;
+                    case '>':
                         j++;
-                    else if(n == 2)
+                        n = 1;
+                        break;
+                    case '<':
                         j--;
-                    else if(n == 3)
+                        n = 2;
+                        break;
+                    case '^':
                         i--;
-                    else if(n == 4)
+                        n = 3;
+                        break;
+                    case 'v':
                         i++;
+                        n = 4;
+                        break;
+                    case '.':
+                        if(n == 1)
+                            j++;
+                        else if(n == 2)
+                            j--;
+                        else if(n == 3)
+                            i--;
+                        else if(n == 4)
+                            i++;
+                        break;
+                }
             }
         }
-        
+        if(i < 0 || i >= h || j < 0 || j >= w)
+            printf("!\n");
+    }
+    else{
+        printf("!\n");
     }
     
- 
     return 0;
 } //Incompleto
