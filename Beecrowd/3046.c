3150c7cd80e2bd1f9884ed9c7f4ccea5744620d2
LuizFernando14 <157256831+LuizFernando14@users.noreply.github.com>
2025-02-02 21:28:45 -0300

    Create 3046.c

---
+++ Beecrowd/3046.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int n, t;
+    scanf("%d", &n);
+    t = ((n+1)*(n+2))/2;
+    printf("%d\n", t);
+    return 0;
+}
